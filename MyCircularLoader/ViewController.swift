0b8a7627edd474806d583c69031eba9477c5e614
Oluwatobi Omotayo <tobitech@ymail.com>
2018-02-08 09:39:19 +0100

    Initial Commit

---
+++ MyCircularLoader/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  MyCircularLoader
+//
+//  Created by Tobi Omotayo on 07/02/2018.
+//  Copyright © 2018 Oluwatobi Omotayo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let shapeLayer = CAShapeLayer()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // let's start by drawing a circle somehow
+        
+        let center = view.center
+        
+        // create my track layer
+        let trackLayer = CAShapeLayer()
+        
+        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
+        
+        trackLayer.path = circularPath.cgPath
+        trackLayer.fillColor = UIColor.clear.cgColor
+        trackLayer.strokeColor = UIColor.lightGray.cgColor
+        trackLayer.lineWidth = 10
+        trackLayer.lineCap = kCALineCapRound
+        view.layer.addSublayer(trackLayer)
+        
+//        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
+        shapeLayer.path = circularPath.cgPath
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.strokeColor = UIColor.red.cgColor
+        shapeLayer.lineWidth = 10
+        
+        shapeLayer.lineCap = kCALineCapRound
+        
+        shapeLayer.strokeEnd = 0
+        
+        view.layer.addSublayer(shapeLayer)
+        
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+        
+    }
+    
+    @objc private func handleTap() {
+        
+        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        basicAnimation.toValue = 1
+        
+        basicAnimation.duration = 2
+        
+        basicAnimation.fillMode = kCAFillModeForwards
+        basicAnimation.isRemovedOnCompletion = false
+        
+        shapeLayer.add(basicAnimation, forKey: "urSoBasic")
+        
+    }
+
+
+}
+
